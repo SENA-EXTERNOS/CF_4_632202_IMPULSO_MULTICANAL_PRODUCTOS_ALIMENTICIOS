e16f71fd1741a99893bdba5001b8d930ecb9bfa7
ricardog07 <71521082+ricardog07@users.noreply.github.com>
2023-01-26 16:28:09 -0500

    Se vinculan los dos videos.

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -24,7 +24,7 @@
       
     .video
       figure
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/FWs9xNZcgBw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -226,8 +226,8 @@
 
     .row.mb-5
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption VIDEO   
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/YzMfN8fHlP8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption Video: Canales de distribución y comercialización según la tecnología para la venta
 
     separador
 
